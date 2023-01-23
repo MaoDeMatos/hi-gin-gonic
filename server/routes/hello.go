0d520574b3aa9b19c498af762894c4e1fd6cd7f5
Mao De Matos <mao.dematos@gmail.com>
2023-01-23 23:56:13 +0100

    ✏️ Fix response on `/hello/:name` route

--- server/routes/hello.go
+++ server/routes/hello.go
@@ -16,7 +16,7 @@ func AddHello(app *gin.Engine) {
 func greet(ctx *gin.Context) {
 	user := ctx.Params.ByName("name")
 	if len(user) > 0 {
-		ctx.JSON(http.StatusOK, gin.H{"data": user})
+		ctx.JSON(http.StatusOK, gin.H{"data": "Hello " + user + "!"})
 	} else {
 		ctx.JSON(http.StatusOK, gin.H{"data": "Hello World!"})
 	}
