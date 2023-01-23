db75ce5b94a70fa66af16d1045e368a29298e0b5
Mao De Matos <mao.dematos@gmail.com>
2023-01-24 00:10:35 +0100

    ✨ Add basic error handling

--- server/router.go
+++ server/router.go
@@ -14,4 +14,18 @@ func createRoutes(app *gin.Engine) {
 
 	routes.AddDelayed(app)
 	routes.AddHello(app)
+
+	app.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(
+			http.StatusNotFound,
+			gin.H{"error": "NOT_FOUND", "message": "This endpoint does not exist"},
+		)
+	})
+
+	app.NoMethod(func(ctx *gin.Context) {
+		ctx.JSON(
+			http.StatusMethodNotAllowed,
+			gin.H{"error": "METHOD_NOT_ALLOWED", "message": "This method is not allowed on this endpoint"},
+		)
+	})
 }
--- server/server.go
+++ server/server.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"hi-gin-gonic/config"
+	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +15,26 @@ func Init() {
 	app := gin.Default()
 	createRoutes(app)
 
+	app.Use(handleErrors)
+
 	// Listen and Server in 0.0.0.0:8080
 	app.Run(config.Current.Port)
+}
+
+func handleErrors(ctx *gin.Context) {
+	log.Printf("Total Errors -> %d", len(ctx.Errors))
+
+	if len(ctx.Errors) <= 0 {
+		ctx.Next()
+		return
+	}
+
+	for _, err := range ctx.Errors {
+		log.Printf("Error -> %+v\n", err)
+	}
+
+	ctx.JSON(
+		http.StatusInternalServerError,
+		gin.H{"error": "INTERNAL_SERVER_ERROR", "data": ctx.Errors},
+	)
 }
